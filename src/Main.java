a68cb4316253b74e5fece7d8e40163a8845b931c
GiuseppeAverna <151162626+GiuseppeAverna@users.noreply.github.com>
2024-03-04 16:38:55 +0100

    esercizio 1 back end

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.SortedMap;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+public class Main{
+    public static void main(String[] args) {
+        int num1 = 6;
+        int num2 = 2;
+
+        int result = mul(num1, num2);
+
+        System.out.println("Il risultato della moltiplicazione è: " + result);
+
+
+
+
+
+
+
+
+    }
+
+    public static int mul(int num1, int num2) {
+        return num1 * num2;
+    }
+
+
+
+
+
+}
+
+
